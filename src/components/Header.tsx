6b5dae47b75e0b1bef6176d5cfb84fe6b683deb2
Seb5000 <sebastian.villarpando@gmail.com>
2025-08-16 12:21:22 -0400

    se movio el sidebar a la derecha

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Header.module.css';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectIsLoggedIn, selectAuth, logOut } from '../store/auth/authSlice';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 interface HeaderProps {
   toggleSidebar: () => void;
@@ -38,55 +38,6 @@ export const Header: React.FC<HeaderProps> = ({
   };
   return (
     <header className={styles.header}>
-      <button
-        className={styles.menuToggle}
-        onClick={toggleSidebar}
-        aria-label={isSidebarOpen ? 'Close sidebar' : 'Open sidebar'}
-      >
-        {isSidebarOpen ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="26"
-            height="26"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <rect
-              x="2"
-              y="2"
-              width="20"
-              height="20"
-              rx="4"
-              ry="4"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="1.5"
-              opacity="0.3"
-            />
-            <path d="M13 16l-4-4 4-4" strokeWidth="2.5" />
-          </svg>
-        ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <line x1="3" y1="12" x2="21" y2="12" />
-            <line x1="3" y1="6" x2="21" y2="6" />
-            <line x1="3" y1="18" x2="21" y2="18" />
-          </svg>
-        )}
-      </button>
       <a className={styles.logo}>Tu voto decide</a>
       <div className={styles.headerActions}>
         {isLoggedIn ? (
@@ -114,10 +65,59 @@ export const Header: React.FC<HeaderProps> = ({
           </>
         ) : (
           <>
-            <Link to="/login">Login</Link>
+            {/* <Link to="/login">Login</Link> */}
             {/* <Link to="/crearCuenta">Crear Cuenta</Link> */}
           </>
         )}
+        <button
+          className={styles.menuToggle}
+          onClick={toggleSidebar}
+          aria-label={isSidebarOpen ? 'Close sidebar' : 'Open sidebar'}
+        >
+          {isSidebarOpen ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="26"
+              height="26"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <rect
+                x="2"
+                y="2"
+                width="20"
+                height="20"
+                rx="4"
+                ry="4"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                opacity="0.3"
+              />
+              <path d="M11 16l4-4-4-4" strokeWidth="2.5" />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <line x1="3" y1="12" x2="21" y2="12" />
+              <line x1="3" y1="6" x2="21" y2="6" />
+              <line x1="3" y1="18" x2="21" y2="18" />
+            </svg>
+          )}
+        </button>
       </div>
     </header>
   );
