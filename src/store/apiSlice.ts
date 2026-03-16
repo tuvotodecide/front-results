115100dd8fb86a738af832bf7b8d4092b3df5da5
Jose Mendoza <joseemendozaan1@gmail.com>
2026-03-16 09:13:20 -0400

    session time extended

--- src/store/apiSlice.ts
+++ src/store/apiSlice.ts
@@ -4,7 +4,7 @@ import {
   createApi,
   fetchBaseQuery,
 } from "@reduxjs/toolkit/query/react";
-// import { logOut } from "./auth/authSlice";
+import { logOut } from "./auth/authSlice";
 
 const { VITE_BASE_API_URL } = import.meta.env;
 const baseApiUrl = VITE_BASE_API_URL || "http://localhost:3000/api/v1";
@@ -70,12 +70,12 @@ const baseQueryWrapper = async (
 
   const result = await baseQuery(adjusted, api, extraOptions);
 
-  // if (result.error?.status === 401) {
-  //   api.dispatch(logOut());
-  //   if (typeof window !== "undefined") {
-  //     window.location.assign("/login");
-  //   }
-  // }
+  if (result.error?.status === 401 && state?.auth?.token) {
+    api.dispatch(logOut());
+    if (typeof window !== "undefined" && window.location.pathname !== "/login") {
+      window.location.assign("/login");
+    }
+  }
   return result;
 };
 
